8644f215ccce324d28286b82850c65edfcd93354
caasI25Vilen <357ghost25Overlord>
2019-10-09 23:03:50 +0400

    adding two classes BST and BST in BST

---
+++ bst_in_bst/bst_in_bst.cpp
@@ -0,0 +1,285 @@
+#include "bst_in_bst.h"
+
+/////////////////////////////////////  ** BST **  /////////////////////////////////////
+
+/*
+ *	Definition of Public Methods
+ */
+
+template<typename T>
+BST<T>::BST()
+	: root_(nullptr) {}
+
+template<typename T>
+BST<T>::BST(const BST& src)
+{
+	root_ = clone(src.root_);
+}
+
+template<typename T>
+BST<T>::~BST()
+{
+	makeEmpty();
+}
+
+template<typename T>
+const T& BST<T>::findMin() const
+{
+	return findMin(root_)->element_;
+}
+
+template<typename T>
+const T& BST<T>::findMax() const
+{
+	return findMax(root_)->element_;
+}
+
+template<typename T>
+bool BST<T>::contain(const T& item) const
+{
+	return contain(item, root_);
+}
+
+template<typename T>
+bool BST<T>::isEmpty() const
+{
+	return root_ == nullptr;
+}
+
+template<typename T>
+void BST<T>::makeEmpty()
+{
+	makeEmpty(root_);
+}
+
+template<typename T>
+void BST<T>::insert(const T& item)
+{
+	insert(item, root_);
+}
+
+template<typename T>
+void BST<T>::remove(const T& item)
+{
+	remove(item, root_);
+}
+
+
+/*
+ *	Definition of Private Methods
+ */
+
+template<typename T>
+void BST<T>::insert(const T& item, BinaryNode*& node)
+{
+	if (node == nullptr)
+		node = new BinaryNode(item, nullptr, nullptr);
+	else if (item <= node->element_)
+		insert(item, node->left_);
+	else if (item > node->element_)
+		insert(item, node->right_);
+	else
+		;
+}
+
+template<typename T>
+void BST<T>::remove(const T& item, BinaryNode* node)
+{
+	if (node == nullptr)
+		return;
+	if (item < node->element_)
+		remove(item, node->left_);
+	else if (item > node->element_)
+		remove(item, node->right_);
+	else if (node->left_ != nullptr && node->right_ != nullptr) // node has 2 children
+	{
+		node->element_ = findMin(node->right_)->element_;
+		remove(node->element_, node->right_);
+	}
+	else
+	{
+		BinaryNode* temp = node;
+		node = (node->left_ != nullptr) ? node->left_ : node->right_;
+		delete temp;
+		temp = nullptr;
+	}
+}
+
+template<typename T>
+typename BST<T>::BinaryNode* BST<T>::findMin(BinaryNode* node) const
+{
+	//** RECURISVE **//
+
+	if (node == nullptr)
+		return nullptr;
+	if (node->left_ == nullptr)
+		return node;
+	return findMin(node->left_);
+
+	//** Iterative **//
+	/*
+	if (node != nullptr)
+		while (node->left_ != nullptr)
+			node = node->left_;
+	return node;
+	*/
+}
+
+template<typename T>
+typename BST<T>::BinaryNode* BST<T>::findMax(BinaryNode* node) const
+{
+	//** Recurisve ** //
+
+	if (node == nullptr)
+		return nullptr;
+	if (node->right_ == nullptr)
+		return node;
+	return findMax(node->right_);
+
+	//** Iterative **//
+	/*
+	if (node == nullptr)
+		while (node->right_ != nullptr)
+			node = node->right_;
+	return node;
+	*/
+}
+
+template<typename T>
+bool BST<T>::contain(const T& item, BinaryNode* node) const
+{
+	if (node == nullptr)
+		return false;
+	if (item < node->element_)
+		return contain(item, node->left_);
+	else if (item > node->element_)
+		return contain(item, node->right_);
+	else
+		return true;
+}
+
+template<typename T>
+void BST<T>::makeEmpty(BinaryNode*& node)
+{
+	if (node != nullptr)
+	{
+		makeEmpty(node->left_);
+		makeEmpty(node->right_);
+		delete node;
+	}
+	node = nullptr;
+}
+
+template<typename T>
+typename BST<T>::BinaryNode* BST<T>::clone(BinaryNode* node) const
+{
+	if (node == nullptr)
+		return nullptr;
+	else
+		return new BinaryNode(node->element, clone(node->left_), clone(node->right_));
+}
+
+
+
+/////////////////////////////////////  ** BST in BST **  /////////////////////////////////////
+
+template<typename T>
+BST_BST<T>::BST_BST(const unsigned short int& elemNumb)
+	: BST_root(nullptr), BST_Node::BN_root(nullptr), BST_Node::counter_(elemNumb) {}
+
+template<typename T>
+BST_BST<T>::BST_BST(const BST_BST& src)
+{
+	BST_root = clone(src.BST_root);
+}
+
+template<typename T>
+BST_BST<T>::~BST_BST()
+{
+	makeEmpty();
+}
+
+template<typename T>
+const T& BST_BST<T>::findMin() const
+{
+	return *(findMin(BST_root)->minNode_);
+}
+
+template<typename T>
+const T& BST_BST<T>::findMax() const
+{
+	return *(findMax(BST_root)->maxNode_);
+}
+
+template<typename T>
+bool BST_BST<T>::isEmpty() const
+{
+	return BST_root == nullptr;
+}
+
+template<typename T>
+bool BST_BST<T>::contain(const T& item) const
+{
+	return contain(item, BST_root);
+}
+
+template<typename T>
+void BST_BST<T>::makeEmpty()
+{
+	makeEmpty(BST_root);
+}
+
+template<typename T>
+void BST_BST<T>::insert(const T& item)
+{
+	insert(item, BST_root);
+}
+
+template<typename T>
+void BST_BST<T>::remove(const T& item)
+{
+	remove(item, BST_root);
+}
+
+
+/*
+ *	Definition of Private Methods
+ */
+
+template<typename T>
+void BST_BST<T>::insert(const T& item, BST_Node*& node)
+{
+	if (node == nullptr)
+	{
+		node = new BST_Node(new typename BST_BST::BinaryNode(item, nullptr, nullptr), nullptr, nullptr);
+	}
+	else if (node->minNode_ == nullptr && node->maxNode_ == nullptr && node->counter_ != 0 && node->depth_ != -1)
+	{
+		insert(item, node->BN_root);
+	}
+	else if (node->minNode_ != nullptr && item <= *node->minNode_ && node->counter_ != 0 && node->depth_ != -1)
+	{
+		insert(item, node->BST_left);
+	}
+	else if (node->maxNode_ != nullptr && item > * node->maxNode_ && node->counter_ != 0 && node->depth_ != -1)
+	{
+		insert(item, node->BST_right);
+	}
+	else if (node->counter_ == 0 && node->depth_ == -1 && item >= node->minNode_ && item <= node->maxNode_)
+	{
+
+	}
+	else if (node->counter_ == 0 && node->depth_ == -1 && item <= node->minNode_)
+	{
+
+	}
+	else if (node->counter_ == 0 && node->depth_ == -1 && item >= node->maxNode_)
+	{
+
+	}
+}
+
+/*
+ *  /////////////////////////////////////  ** Nested BST_Node **  /////////////////////////////////////
+ */
+
--- bst_in_bst/bst_in_bst.h
+++ bst_in_bst/bst_in_bst.h
@@ -1,1 +1,113 @@
 #pragma once
+#include<cmath>
+
+/////////////////////////////////////  ** BST **  /////////////////////////////////////
+template<typename T>
+class BST
+{
+public:
+
+	BST();
+	BST(const BST& src);
+	~BST();
+
+	const T& findMin() const;
+	const T& findMax() const;
+	bool contain(const T& item) const;
+	bool isEmpty() const;
+
+	void makeEmpty();
+	void insert(const T& item);
+	void remove(const T& item);
+
+
+private:
+
+	struct BinaryNode
+	{
+		T element_;
+		BinaryNode* left_;
+		BinaryNode* right_;
+
+		BinaryNode(const T& element, BinaryNode* left, BinaryNode* right)
+			: element_(element), left_(left), right_(right) {};
+	};
+
+	BinaryNode* root_;
+
+	void insert(const T& item, BinaryNode*& node);
+	void remove(const T& item, BinaryNode* node);
+	BinaryNode* findMin(BinaryNode* node) const;
+	BinaryNode* findMax(BinaryNode* node) const;
+	bool contain(const T& item, BinaryNode* node) const;
+	void makeEmpty(BinaryNode*& node);
+	BinaryNode* clone(BinaryNode* node) const;
+};
+
+
+
+/////////////////////////////////////  ** BST in BST **  /////////////////////////////////////
+template<typename T>
+class BST_BST
+{
+public:
+	BST_BST(const unsigned short int& elemNumb = 7); // Maximum Depth of Nested Binary Search Tree
+	BST_BST(const BST_BST& src);
+	~BST_BST();
+
+	const T& findMin() const;
+	const T& findMax() const;
+	bool isEmpty() const;
+	bool contain(const T& item) const;
+
+	void makeEmpty();
+	void insert(const T& item);
+	void remove(const T& item);
+
+private:
+	struct BST_Node
+	{
+		struct BinaryNode
+		{
+			T element_;
+			BinaryNode* BN_left;
+			BinaryNode* BN_right;
+
+			BinaryNode(const T& element, BinaryNode* lt, BinaryNode* rt)
+				: element_(element), BN_left(lt), BN_right(rt), counter_(counter_ - 1), depth_(depth_ - 1) {}
+
+		};
+		BinaryNode* BN_root;
+
+		T* minNode_;
+		T* maxNode_;
+		BST_Node* BST_left;;
+		BST_Node* BST_right;
+
+		unsigned short int counter_ = 7;			// Maximum Number of Nodes (Binary Nodes)
+		short int depth_ = log2(counter_ + 1) - 1;	// Maximum Depth of Nested Binary Search Tree
+
+
+		BST_Node(BinaryNode* BNroot, BST_Node* BSTleft, BST_Node* BSTright)
+			: BN_root(BNroot), BST_left(BSTleft), BST_right(BSTright), minNode_(nullptr), maxNode_(nullptr) {}
+
+		bool isFull() const;
+		void insert(const T& item, BinaryNode* node);
+		void remove(const T& item, BinaryNode* node);
+		//BinaryNode* findMin(BinaryNode* node) const;			//**	I think it won't be necessary
+		//BinaryNode* findMax(BinaryNode* node) const;			//**	I think it won't be necessary
+		bool contain(const T& item, BinaryNode* node) const;
+		void makeEmpty(BinaryNode*& node);
+		void clone(BinaryNode* node) const;
+
+	};
+	BST_Node* BST_root;
+
+	void insert(const T& item, BST_Node*& node);
+	void remove(const T& item, BST_Node*& node);
+	BST_Node* findMin(BST_Node* node) const;			//**	I think it won't be necessary
+	BST_Node* findMax(BST_Node* node) const;			//**	I think it won't be necessary
+	bool contain(const T& item, BST_Node* node) const;
+	void makeEmpty(BST_Node*& node);
+	void clone(BST_Node* node) const;
+};
